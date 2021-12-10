6725917bddcf38ba2fa2cf91d2f3b8d0e8a9fb9f
danielalcover1 <1562889@uab.cat>
2021-12-10 18:37:55 +0100

    segon escenari de login incorrecte

--- src/test/java/io/cucumber/skeleton/Login.java
+++ src/test/java/io/cucumber/skeleton/Login.java
@@ -23,14 +23,18 @@ public class Login {
     // This property is optional.
     // If not specified, WebDriver searches the path for chromedriver in your environment variables
     // Example path for Linux or Mac:
+    //ChromeOptions options = new ChromeOptions();
+    //options.addArguments("start-maximized");
+    //System.setProperty("webdriver.chrome.driver", "\\Users\\dania\\Documents\\uni 3\\TQS\\Practica2\\chromedriver.exe");
+    //driver = new ChromeDriver(options);
+  }
+
+  @Given("access to the main page")
+  public void accessToTheMainPage() {
     ChromeOptions options = new ChromeOptions();
     options.addArguments("start-maximized");
     System.setProperty("webdriver.chrome.driver", "\\Users\\dania\\Documents\\uni 3\\TQS\\Practica2\\chromedriver.exe");
     driver = new ChromeDriver(options);
-  }
-
-  @Before
-  public void init(){
     driver.get("https://www.caprabo.com/ca/home/");
   }
 
@@ -52,14 +56,19 @@ public class Login {
     driver.findElement(By.xpath("//*[@class=\"zu-derecha\"]")).click();
     driver.findElement(By.xpath("//*[@id=\"zu_header_boton_login\"]")).click();
     driver.findElement(By.xpath("//*[@id=\"onetrust-accept-btn-handler\"]")).click();
-    String winHandle = driver.getWindowHandle();
-    driver.switchTo().window(winHandle);
   }
 
   @When("login with wrong user and right password")
   public void loginWithWrongUserAndRightPassword() {
+    driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("correuerroni@gmail.com");
+    driver.findElement(By.xpath("//*[@id=\"clave1\"]")).sendKeys("kM~1O2?2");
+  }
+
+  @When("login with right user and wrong password")
+  public void loginWithRightUserAndWrongPassword() {
     driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("danialcover001@gmail.com");
-    driver.findElement(By.xpath("//*[@id=\"clave1\"]")).sendKeys("passworderronia");
+    driver.findElement(By.xpath("//*[@id=\"clave1\"]")).sendKeys("passowrderronia");
+
   }
 
   @Then("the login is not successful")
--- src/test/resources/io/cucumber/skeleton/login.feature
+++ src/test/resources/io/cucumber/skeleton/login.feature
@@ -1,6 +1,14 @@
-Feature: LOGIN
+Feature: login
+
+Background:
+  Given access to the main page
 
 Scenario: insert wrong user
   Given being in the login page
   When login with wrong user and right password
+  Then the login is not successful
+
+Scenario: insert wrong password
+  Given being in the login page
+  When login with right user and wrong password
   Then the login is not successful
